825e902fe6bc395a31e6610df6005d04e9add5a5
Emiliano Foti <foti.emiliano@gmail.com>
2023-12-14 13:16:06 +0100

    iterations done but no bonus

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,29 @@ import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
 import ExponentSix from "./components/ExponentSix";
 
+import { useState } from "react";
+
 
 function App () {
+  const [count, setCount] = useState(0);
+  const decrement = () => setCount((prevCount) => prevCount - 1);
+  const increment = () => setCount((prevCount) => prevCount + 1);
+
   return (
     <div className="App">
       <h2><em>Counter</em></h2>
   
-      <Counter/>
+      <Counter count={count} decrement={decrement} increment={increment} />
 
       <br />
       <h2><em>Exponents</em></h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+        <ExponentTwo count={count} countPow2={count **2}/>
+        <ExponentThree count={count} countPow3={count **3}/>
+        <ExponentFour count={count} countPow4={count **4}/>
+        <ExponentFive count={count} countPow5={count **5}/>
+        <ExponentSix count={count} countPow6={count **6}/>
       </div>
     </div>
   );
--- src/components/ExponentTwo.jsx
+++ src/components/ExponentTwo.jsx
@@ -1,7 +1,7 @@
-const ExponentTwo = () => (
+const ExponentTwo = ({count, countPow2}) => (
    <div className="exponent-counter-container">
-    <p className="exponent-label">n²</p>
-    <p className="exponent-result">2 * 2 = <span className="total">4</span></p>
+    <p className="exponent-label">{count}²</p>
+    <p className="exponent-result">{count} * {count} = <span className="total">{countPow2}</span></p>
   </div>
 );
 
